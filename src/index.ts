9df2f0dae8d70ac64cd1eec05982b757b18354ab
arikacips <1294330981oy@gmail.com>
2025-04-21 13:58:43 +0800

    Latest changes

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ import { createRequire } from 'module';
 
 const require = createRequire(import.meta.url);
 const NHentaiApi = require('nhentai-api');
+// console.error(NHentaiApi);
 
 interface SaucenaoSearchParams {
 	imageUrl?: string;
